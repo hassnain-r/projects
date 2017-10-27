f50cba38bb01eca97b74540e1eb7b2a1db152537
husnainrehman <hussyrehman880@gmail.com>
2017-10-27 12:13:34 +0500

    code according to requirement

--- count.py
+++ count.py
@@ -3,22 +3,13 @@ import json
 import argparse
 
 
-class JsonAnalyzer:
-    def __init__(self, name='jsonanalyzer'):
-        self.name = name
-
-    def countdata(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-f_n', '--filename')
-        args = parser.parse_args()
-        with open(args.filename, 'r') as my_file:
+class JsonAnalyzer(object):
+    def unique_values_and_type(self, is_category=False):
+        with open(args.filename)as my_file:
             content = json.load(my_file)
-            config = len(content)
-            string = 'Total_Rows'
-            print('{} {}'.format(string, config))
+            print('{} {}'.format('Total_Rows', len(content)))
 
-            def get_recursion(json_items, result=None, parent_key=None):
-
+            def nested_dict_iterator(json_items, result=None, parent_key=None):  # recursive func for nested dicts
                 if result is None:
                     result = {}
                 if isinstance(json_items, dict):
@@ -27,95 +18,68 @@ class JsonAnalyzer:
                             result_key = parent_key + '.' + dict_key
                         else:
                             result_key = dict_key
+
                         if result_key not in result:
                             result[result_key] = {}
-                        if "unique" not in result[result_key]:
-                            result[result_key]["unique"] = []
+
                         if "type" not in result[result_key]:
                             result[result_key]["type"] = ""
+                        result[result_key]["type"] = type(dict_value).__name__
+                        if is_category:
+                            if "unique" not in result[result_key]:
+                                result[result_key]["unique"] = []
+                            if dict_value and dict_value not in result[result_key]["unique"]:
+                                result[result_key]["unique"].append(dict_value)
+
                         if "count" not in result[result_key]:
                             result[result_key]["count"] = 0
-                        result[result_key]["type"] = type(dict_value).__name__
+
                         if isinstance(dict_value, int):
                             if dict_value != 0:
                                 result[result_key]["count"] += 1
                         elif len(dict_value) > 0:
                             result[result_key]["count"] += 1
-                        result = get_recursion(dict_value, result, dict_key)
-                if isinstance(json_items, list):
+                        result = nested_dict_iterator(dict_value, result, dict_key)
+
+                elif isinstance(json_items, list):
                     for value in json_items:
-                        result = get_recursion(value, result, parent_key)
+                        result = nested_dict_iterator(value, result, parent_key)
                 return result
+            new_data = nested_dict_iterator(content)
 
-            new_data = get_recursion(content)
-            for final_key, final_value in new_data.items():
-                percentage = final_value['count'] / len(content) * 100
-                type_of_element = (final_value['type'])
-                print (final_key + " = " + str(type_of_element) + "[" + str(percentage) + "]")
+            if is_category:
+                self.final_result_unique_category(new_data)
+            else:
+                self.final_result_all_categories(new_data, len(content))
 
-    def unique_data(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c', '--single_category')
-        parser.add_argument('-f', '--file_name')
-        args = parser.parse_args()
-        args.single_category = ''
-        with open(args.file_name, 'r') as my_file:
-            content = json.load(my_file)
-            config = len(content)
-            string = 'Total_Rows'
-            print('{} {}'.format(string, config))
+    def final_result_unique_category(self, final_data):
+        
+        for final_key, final_value in final_data.items():
+            if args.each_category == final_key:
+                type_of_data = final_value['type']
+                original_count = len(final_value['unique'])
+                total_count = (final_value['count'])
+                percentage = original_count / total_count * 100
+                print (final_key + " = " + str(type_of_data) + '[' + str(percentage) + '% unique]' + "\n"
+                       + str(sorted(final_value['unique'])))
 
-            def get_recursion(json_items, result=None, parent_key=None):
-
-                if result is None:
-                    result = {}
-                if isinstance(json_items, dict):
-                    for dict_key, dict_value in json_items.items():
-                        if parent_key:
-                            result_key = parent_key + '.' + dict_key
-                        else:
-                            result_key = dict_key
-                        if result_key not in result:
-                            result[result_key] = {}
-                        if "unique" not in result[result_key]:
-                            result[result_key]["unique"] = []
-                        if "type" not in result[result_key]:
-                            result[result_key]["type"] = ""
-                        if "count" not in result[result_key]:
-                            result[result_key]["count"] = 0
-                        result[result_key]["type"] = type(dict_value).__name__
-                        if isinstance(dict_value, int):
-                            if dict_value != 0:
-                                result[result_key]["count"] += 1
-                        elif len(dict_value) > 0:
-                            result[result_key]["count"] += 1
-                        if dict_value and dict_value not in result[result_key]["unique"]:
-                            result[result_key]["unique"].append(dict_value)
-                        result = get_recursion(dict_value, result, dict_key)
-
-                if isinstance(json_items, list):
-                    for value in json_items:
-                        result = get_recursion(value, result, parent_key)
-                return result
-
-            new_data = get_recursion(content)
-            for final_key, final_value in new_data.items():
-                if args1.single_category == final_key:
-                    type_of_data = final_value['type']
-                    original_count = len(final_value['unique'])
-                    total_count = (final_value['count'])
-                    percentage = original_count / total_count * 100
-                    print (final_key + " = " + str(type_of_data) + '[' + str(percentage) + '% unique]' + str(final_value['unique']))
+    def final_result_all_categories(self, final_data, content_length):
+        for final_key, final_value in final_data.items():
+            percentage = final_value['count'] / content_length * 100
+            type_of_element = (final_value['type'])
+            print (final_key + " = " + str(type_of_element) + "[" + str(percentage) + "]")
 
 if __name__ == '__main__':
-    s = JsonAnalyzer()
+    my_class = JsonAnalyzer()
     parsers = argparse.ArgumentParser()
-    parsers.add_argument('-c', '--single_category')
-    parsers.add_argument('-f', '--file_name')
-    parsers.add_argument('-f_n', '--filename')
-    args1 = parsers.parse_args()
-    if args1.single_category:
-        if args1.file_name:
-            print (s.unique_data())
-    elif args1.filename:
-        print (s.countdata())
+    parsers.add_argument('-f', '--filename')
+    parsers.add_argument('-c', '--each_category')
+    args = parsers.parse_args()
+    if args.each_category:
+        if args.filename:
+            print (my_class.unique_values_and_type(True))
+    elif args.filename:
+        print (my_class.unique_values_and_type(False))
+
+
+
